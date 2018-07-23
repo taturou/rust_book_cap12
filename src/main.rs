c1bc9f34138b7a60958e3cd590f45d2d465d4c9d
taturou <taturou@gmail.com>
2018-07-23 23:54:51 +0900

    The book 2nd: list 12-7

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for \"{}\" in file \"{}\"", config.query, config.filename);
 
@@ -17,12 +17,14 @@ fn main() {
 }
 
 struct Config {
-  query: String,
-  filename: String,
+    query: String,
+    filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {query, filename}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
 }
