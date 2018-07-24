c75249340115b84309b22613abee0af7ebe56e83
h-morishita <h-morishita@esol.co.jp>
2018-07-24 21:43:40 +0900

    The book 2nd: list 12-16

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,25 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents));
+    }
+}
